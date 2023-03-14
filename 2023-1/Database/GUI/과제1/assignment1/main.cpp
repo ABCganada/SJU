fb82cef608661eca9878fcc5f19f57866d336f5b
ABCganada <shinmg9712@gmail.com>
2023-03-14 21:26:01 +0900

    GUI - File Save, Insert, program exit

--- 2023-1/Database/GUI/과제1/assignment1/main.cpp
+++ 2023-1/Database/GUI/과제1/assignment1/main.cpp
@@ -12,11 +12,13 @@
 #include <QRegularExpression>
 #include <QRegularExpressionMatch>
 
-void fileLoad(QTextEdit *textEdit);     // file Load 함수
-void filePrint(QTextEdit *textEdit);    // file Print 함수
-void fileUpdate(QTextEdit *textEdit);   // file Update 함수
-void fileDelete(QTextEdit *textEdit);   // file Delete 함수
-void fileFind(QTextEdit *textEdit);     // file Find 함수
+void fileLoad(QTextEdit *text);     // file Load 함수
+void filePrint(QTextEdit *text);    // file Print 함수
+void fileUpdate(QTextEdit *text);   // file Update 함수
+void fileDelete(QTextEdit *text);   // file Delete 함수
+void fileFind(QTextEdit *text);     // file Find 함수
+void fileSave(QTextEdit *textEdit);     // file Save 함수
+void fileInsert(QTextEdit *text);   // file Insert 함수
 
 int main(int argc, char *argv[])
 {
@@ -44,17 +46,20 @@ int main(int argc, char *argv[])
     QAbstractButton::connect(deleteButton, &QPushButton::clicked, [textEdit]() {fileDelete(textEdit); });
     layout->addWidget(deleteButton);
 
-    QPushButton *findButton = new QPushButton("File Find"); // file Delete 기능 버튼
+    QPushButton *findButton = new QPushButton("File Find"); // file Find 기능 버튼
     QAbstractButton::connect(findButton, &QPushButton::clicked, [textEdit]() {fileFind(textEdit); });
     layout->addWidget(findButton);
 
-    QPushButton *insertButton = new QPushButton("File Insert"); // file Delete 기능 버튼
-    layout->addWidget(insertButton);
-
-    QPushButton *saveButton = new QPushButton("File Save"); // file Delete 기능 버튼
+    QPushButton *saveButton = new QPushButton("File Save"); // file Save 기능 버튼
+    QAbstractButton::connect(saveButton, &QPushButton::clicked, [textEdit]() {fileSave(textEdit); });
     layout->addWidget(saveButton);
 
-    QPushButton *exitButton = new QPushButton("Exit"); // file Delete 기능 버튼
+    QPushButton *insertButton = new QPushButton("File Insert"); // file Insert 기능 버튼
+    QAbstractButton::connect(insertButton, &QPushButton::clicked, [textEdit]() {fileInsert(textEdit); });
+    layout->addWidget(insertButton);
+
+    QPushButton *exitButton = new QPushButton("Exit"); // program Exit 기능 버튼
+    QAbstractButton::connect(exitButton, &QPushButton::clicked, &app, QApplication::quit);  // 클릭시 app 종료
     layout->addWidget(exitButton);
 
 
@@ -64,7 +69,7 @@ int main(int argc, char *argv[])
     return app.exec();  // app 실행
 }
 
-void fileLoad(QTextEdit *textEdit){
+void fileLoad(QTextEdit *text){
     // QFileDialog : 파일을 선택하는 GUI Interface를 제공
     // getOpenFileName() -> nullptr : 부모 위젯 없음. 2,3번째 argument : 메세지. 4번째 : 텍스트 파일을 가져올 거임.
     QString fileName = QFileDialog::getOpenFileName(nullptr, "Open File", "", "Text Files (*.txt)");
@@ -81,15 +86,15 @@ void fileLoad(QTextEdit *textEdit){
 
     // QTextStream : 텍스트를 읽고 쓰기 위한 Interface 제공
     QTextStream in(&file); // 성공적으로 파일을 로드하면 QFile의 file을 QTextStream in에 저장.
-    textEdit->setText(in.readAll());    // QTextEdit의 textEdit 위젯에 QTextStream의 in의 콘텐츠 전체를 세팅.
+    text->setText(in.readAll());    // QTextEdit의 textEdit 위젯에 QTextStream의 in의 콘텐츠 전체를 세팅.
     QMessageBox::information(nullptr, "~ SUCCESS", "File Load !");
 }
-void filePrint(QTextEdit *textEdit){
+void filePrint(QTextEdit *text){
     // toPlainText() : TextEdit의 글자들을 가져옴.
-    QString contents = textEdit->toPlainText(); // toPlainText() 메서드를 사용해 QTextEdit 위젯의 텍스트 내용을 가져와 contents에 저장.
+    QString contents = text->toPlainText(); // toPlainText() 메서드를 사용해 QTextEdit 위젯의 텍스트 내용을 가져와 contents에 저장.
     QMessageBox::information(nullptr, "~ SUCCESS : File Print", contents);   // 메세지 박스를 열어 contents를 표시함.
 }
-void fileUpdate(QTextEdit *textEdit){
+void fileUpdate(QTextEdit *text){
     // QInputDialog : User로부터 text, 정수 등을 입력 받음.
     // getText() : text를 받겠다.
     QString target = QInputDialog::getText(nullptr, "Target", "");  // 타겟 string 입력
@@ -102,41 +107,73 @@ void fileUpdate(QTextEdit *textEdit){
         return;
     }
 
-    QString contents = textEdit->toPlainText(); // 함수 파라미터 textEdit의 텍스트 내용을 contents에 저장.
+    QString contents = text->toPlainText(); // 함수 파라미터 textEdit의 텍스트 내용을 contents에 저장.
     contents.replace(target, replace);  // contents 속 target을 replace로 대체
 
     // QTextCursor : QTextDocuments에 액세스하고 수정할 수 있는 API 제공.
-    QTextCursor cursor = textEdit->textCursor();    // cursor가 현재 textEdit에 액세스함.
+    QTextCursor cursor = text->textCursor();    // cursor가 현재 textEdit에 액세스함.
     cursor.select(QTextCursor::Document);   // 현재 Document를 select함.
     cursor.removeSelectedText();    // seleced text를 지움.
     cursor.insertText(contents);    // 대체된 contents를 cursor에 넣어 수정함.
 
     QMessageBox::information(nullptr, "~ SUCCESS", "File Update!");
 }
-void fileDelete(QTextEdit *textEdit){
+void fileDelete(QTextEdit *text){
     QString del = QInputDialog::getText(nullptr, "Delete", "");
-    QString contents = textEdit->toPlainText();
+    QString contents = text->toPlainText();
+    if(del.isEmpty()){
+        return;
+    }
     contents.remove(del);
-    textEdit->setText(contents);
+    text->setText(contents);
 }
-void fileFind(QTextEdit *textEdit)
-{
-    QString textContent = textEdit->toPlainText();
-    QString keyword = QInputDialog::getText(nullptr, "Find", "");
-    QStringList searchResults;
+void fileFind(QTextEdit *text){
+    QString contents = text->toPlainText();
+    QString find = QInputDialog::getText(nullptr, "Find", "");   // 찾을 단어
+    QStringList results;
 
-    QRegularExpression re(keyword, QRegularExpression::CaseInsensitiveOption);
-    QTextStream textStream(&textContent);
+    QRegularExpression re(find, QRegularExpression::CaseInsensitiveOption);
+    QTextStream textStream(&contents);
+
     while (!textStream.atEnd()) {
         QString line = textStream.readLine();
         QRegularExpressionMatch match = re.match(line);
         if (match.hasMatch()) {
-            searchResults.append("FOUND : " + line);
+            results.append("FOUND : " + line);
         }
     }
 
-    if (searchResults.isEmpty())
-        QMessageBox::information(nullptr, "Not Found", "The text could not be found.");
+    if (results.isEmpty())
+        QMessageBox::information(nullptr, "~ FAIL", "File Find");
     else
-        QMessageBox::information(nullptr, "Search Results", searchResults.join("\n"));
+        QMessageBox::information(nullptr, "~ SUCCESS", results.join("\n"));
+}
+void fileSave(QTextEdit *text)
+{
+    QString newFile = QFileDialog::getSaveFileName(nullptr, "Save File", "", "Text files (*.txt)");
+
+    if (newFile.isEmpty())
+        return;
+
+    QFile file(newFile);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QMessageBox::warning(nullptr, "Error", "Failed to save file.");
+        return;
+    }
+
+    QTextStream out(&file);
+    out << text->toPlainText();
+
+    file.close();
+
+    QMessageBox::information(nullptr, "File Saved", "The file has been saved.");
+}
+void fileInsert(QTextEdit *text){
+    QString insert = QInputDialog::getText(nullptr, "Insert; append text", "");
+    QString contents = text->toPlainText();
+    if(insert.isEmpty()){
+        return;
+    }
+    contents.append("\n" + insert);
+    text->setText(contents);
 }
