ad8d4a9d795f6ff82c6d5c001b994e5cad1c055d
ABCganada <shinmg9712@gmail.com>
2023-04-07 14:03:36 +0900

    DS - Linked List2 / remember lastNode in result

--- 2023-1/Datastructure/5_linkedList2/prob1.c
+++ 2023-1/Datastructure/5_linkedList2/prob1.c
@@ -13,7 +13,7 @@ typedef struct __list{      // header 가지고 있는 리스트
 }List;
 
 void init(List *plist);     // List 초기화
-void appendTerm(List *plist, int coef, int exp);    // 입력값 단일연결리스트 연결
+void appendTerm(Node *lastNode, int coef, int exp);    // 입력값 단일연결리스트 연결
 List addPoly(List *plist1, List *plist2);           // 다항식 덧셈
 Node *getNode();        // node 생성
 void freeList(List *plist); // List의 메모리 해제
@@ -21,22 +21,26 @@ void freeList(List *plist); // List의 메모리 해제
 int main()
 {
     List list1, list2, result;    // 다항식 두개
-    Node *ptrNode = NULL;
+    Node *lastNode, *ptrNode = NULL;
     init(&list1);
     init(&list2);
 
     int i, n1, n2, coef, exp;
 
     scanf("%d", &n1);                   // 1번째 다항식 항의 개수
+    lastNode = list1.header;
     for(i=0; i<n1; i++){
         scanf("%d %d", &coef, &exp);
-        appendTerm(&list1, coef, exp);  // 항 추가 함수 호출
+        appendTerm(lastNode, coef, exp);  // 항 추가 함수 호출
+        lastNode = lastNode->next;
     }
 
     scanf("%d", &n2);                   // 2번째 다항식 항의 개수
+    lastNode = list2.header;
     for(i=0; i<n2; i++){     
         scanf("%d %d", &coef, &exp);
-        appendTerm(&list2, coef, exp);  // 항 추가 함수 호출
+        appendTerm(lastNode, coef, exp);  // 항 추가 함수 호출
+        lastNode = lastNode->next;
     }
 
     result = addPoly(&list1, &list2);   // result List에 다항식 덧셈 리턴값 저장
@@ -60,16 +64,12 @@ void init(List *plist){
 
     plist->header = header;
 }
-void appendTerm(List *plist, int coef, int exp){
+void appendTerm(Node *lastNode, int coef, int exp){
     Node *newNode = getNode();
     newNode->coef = coef;
     newNode->exp = exp;
 
-    Node *ptrNode = plist->header;
-    while(ptrNode->next != NULL){
-        ptrNode = ptrNode->next;
-    }
-    ptrNode->next = newNode;
+    lastNode->next = newNode;
 }
 List addPoly(List *plist1, List *plist2){
     List result;
@@ -82,28 +82,34 @@ List addPoly(List *plist1, List *plist2){
 
     while(p1 != NULL && p2 != NULL){
         if(p1->exp > p2->exp){
-            appendTerm(&result, p1->coef, p1->exp);
+            appendTerm(r, p1->coef, p1->exp);
             p1 = p1->next;
+            r = r->next;
         }
         else if(p1->exp < p2->exp){
-            appendTerm(&result, p2->coef, p2->exp);
+            appendTerm(r, p2->coef, p2->exp);
             p2 = p2->next;
+            r = r->next;
         }
         else{
             int sumCoef = p1->coef + p2->coef;
-            if(sumCoef != 0)
-                appendTerm(&result, sumCoef, p1->exp);
+            if(sumCoef){
+                appendTerm(r, sumCoef, p1->exp);
+                r = r->next;
+            }
             p1 = p1->next;
             p2 = p2->next;
         }
     }
     while(p1 != NULL){
-        appendTerm(&result, p1->coef, p1->exp);
+        appendTerm(r, p1->coef, p1->exp);
         p1 = p1->next;
+        r = r->next;
     }
     while(p2 != NULL){
-        appendTerm(&result, p2->coef, p2->exp);
+        appendTerm(r, p2->coef, p2->exp);
         p2 = p2->next;
+        r = r->next;
     }
 
     return result;
