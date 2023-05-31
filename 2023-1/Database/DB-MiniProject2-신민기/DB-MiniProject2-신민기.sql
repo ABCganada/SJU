5b75c88c13e855dc45576f5f8e0fa7d2d732691e
ABCganada <shinmg9712@gmail.com>
2023-05-31 22:31:21 +0900

    commit

--- 2023-1/Database/DB-MiniProject2-신민기/DB-MiniProject2-신민기.sql
+++ 2023-1/Database/DB-MiniProject2-신민기/DB-MiniProject2-신민기.sql
@@ -29,16 +29,14 @@ CREATE TABLE driver (
   FOREIGN KEY (carno) REFERENCES car (id) on delete cascade
 );
 -------------------------
-create table customer(
-  id int not null,
-  name varchar(255),
-  address varchar(255),
-  phone_no varchar(20),
-  email varchar(255),
-  dno int,
-
-  primary key(id),
-  foreign key(dno) references driver(id)
+CREATE TABLE customer (
+  id INT IDENTITY(1,1) PRIMARY KEY,
+  name VARCHAR(255),
+  address VARCHAR(255),
+  phone_no VARCHAR(20),
+  email VARCHAR(255),
+  dno INT,
+  FOREIGN KEY (dno) REFERENCES driver(id)
 );
 --------------------------
 create table package(
@@ -77,27 +75,27 @@ insert into car values(10, 100, 5, 8282);
 insert into car values(11, 100, 5, 666);
 insert into car values(12, 200, 10, 1303);
 --------------------------
-insert into driver values(111, 'Jung', '0001-0111', 0, 0, 1);
-insert into driver values(112, 'Kang', '0002-0112', 0, 0, 2);
-insert into driver values(113, 'Yoon', '0003-0113', 0, 0, 3);
-insert into driver values(114, 'Jang', '0022-0114', 0, 0, 4);
+insert into driver values(111, 'Jung', '0001-0111', 0, 25, 1);
+insert into driver values(112, 'Kang', '0002-0112', 0, 50, 2);
+insert into driver values(113, 'Yoon', '0003-0113', 0, 30, 3);
+insert into driver values(114, 'Jang', '0022-0114', 0, 40, 4);
 insert into driver values(115, 'Jung', '0031-0115', 0, 0, 5);
-insert into driver values(116, 'Kang', '0041-0116', 0, 0, 6);
-insert into driver values(117, 'Kang', '0777-0117', 0, 0, 7);
-insert into driver values(118, 'Han', '0777-0118', 0, 0, 8);
-insert into driver values(119, 'Seo', '8282-0119', 0, 0, 10);
-insert into driver values(120, 'An', '1303-0120', 0, 0, 12);
+insert into driver values(116, 'Kang', '0041-0116', 0, 2, 6);
+insert into driver values(117, 'Kang', '0777-0117', 0, 5, 7);
+insert into driver values(118, 'Han', '0777-0118', 0, 9, 8);
+insert into driver values(119, 'Seo', '8282-0119', 0, 1, 10);
+insert into driver values(120, 'An', '1303-0120', 0, 100, 12);
 --------------------------
-insert into customer values(1, 'KIM', 'seoul', '010-1234-5678', 'kim123@gmail.com', 111);
-insert into customer values(2, 'LEE', 'seoul', '010-1111-2222', 'lee0107@email.com', 111);
-insert into customer values(3, 'PARK', 'busan', '010-2345-7890', 'phd33@gmail.com', 113);
-insert into customer values(4, 'CHOI', 'gwangju', '010-7272-8282', 'ch8802@email.com', 114);
-insert into customer values(5, 'SHIN', 'yongin', '010-3008-3613', 'smg9712@gmail.com', 112);
-insert into customer values(6, 'KIM', 'seoul', '010-1235-5321', 'kim123@naver.com', 111);
-insert into customer values(7, 'CHO', 'daejeon', '010-4567-8901', 'cho3mo4@naver.com', 116);
-insert into customer values(8, 'KIM', 'incheon', '010-3333-2211', 'kjs99@email.com', 119);
-insert into customer values(9, 'LEE', 'suwon', '010-4033-7692', 'ljh0827@naver.com', 112);
-insert into customer values(10, 'OH', 'jeju', '010-3577-7692', 'cjamm@email.com', 120);
+insert into customer(name, address, phone_no, email, dno) values('KIM', 'seoul', '010-1234-5678', 'kim123@gmail.com', 111);
+insert into customer(name, address, phone_no, email, dno) values('LEE', 'seoul', '010-1111-2222', 'lee0107@email.com', 111);
+insert into customer(name, address, phone_no, email, dno) values('PARK', 'busan', '010-2345-7890', 'phd33@gmail.com', 113);
+insert into customer(name, address, phone_no, email, dno) values('CHOI', 'gwangju', '010-7272-8282', 'ch8802@email.com', 114);
+insert into customer(name, address, phone_no, email, dno) values('SHIN', 'yongin', '010-3008-3613', 'smg9712@gmail.com', 112);
+insert into customer(name, address, phone_no, email, dno) values('KIM', 'seoul', '010-1235-5321', 'kim123@naver.com', 111);
+insert into customer(name, address, phone_no, email, dno) values('CHO', 'daejeon', '010-4567-8901', 'cho3mo4@naver.com', 116);
+insert into customer(name, address, phone_no, email, dno) values('KIM', 'incheon', '010-3333-2211', 'kjs99@email.com', 119);
+insert into customer(name, address, phone_no, email, dno) values('LEE', 'suwon', '010-4033-7692', 'ljh0827@naver.com', 112);
+insert into customer(name, address, phone_no, email, dno) values('OH', 'jeju', '010-3577-7692', 'cjamm@email.com', 120);
 --------------------------
 insert into package values(1, 1111, 3, default, default);
 insert into package values(1, 1112, 5, default, default);
@@ -122,23 +120,13 @@ where customer_id in (
 );
 -------------------------
 update driver
-set total_packs =(
-	select count(*)
-	from package
-	join customer on package.customer_id = customer.id
-	where customer.dno = driver.id
-);
+set total_packs = total_packs + (select count(*) from customer, package where driver.id=customer.dno and customer.id=package.customer_id and package.status <> 1);
 -------------------------
 select id, (select count(*) from customer where customer.dno = driver.id) as total_customer
 from driver;
 -------------------------
 update driver
-set packs =(
-	select count(*)
-	from package
-	join customer on package.customer_id = customer.id
-	where customer.dno = driver.id and package.status != 5
-);
+set packs = (select count(*) from customer, package where driver.id=customer.dno and customer.id=package.customer_id and package.status <> 1);
 -------------------------
 select name, address, phone_no
 from customer
@@ -158,3 +146,18 @@ from package
 update package
 set status = 1
 where customer_id = 10 and pno = 1111;
+-------------------------
+select lb.id, lb.name, hb.id, hb.name
+from branch as lb, branch as hb
+where lb.manager = hb.id;
+-------------------------
+select id, name,, phone_no, email
+from customer
+order by name asc;
+-------------------------
+SELECT id, name, CASE
+  WHEN total_packs >= 5 THEN 2500000 + (15000 * total_packs)
+  ELSE 2500000
+  END AS Salary
+FROM driver
+order by name;
