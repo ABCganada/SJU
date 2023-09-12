cb95ee3e7b444f3f04d36c4416fef73acd92b9b3
ABCganada <shinmg9712@gmail.com>
2023-09-12 13:37:55 +0900

    알고리즘 - 선택 정렬/prob1

---
+++ 2023-2/algorithm/2_priortyQueue/prob1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b);
+int findMaxIdx(int *arr, int startIdx, int endIdx);
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(sizeof(int) * n);
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int pos = n-1;
+    for(int i=0; i<n-1; i++){
+        int swapPos = findMaxIdx(arr, 1, n-i);
+        swap(&arr[swapPos], &arr[pos]);
+        pos--;
+    }
+
+    for(int i=0; i<n; i++){
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
+
+    return 0;
+}
+void swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+int findMaxIdx(int *arr, int startIdx, int endIdx){
+    int max = arr[0];
+    int ret = 0;
+
+    for(int i=startIdx; i<endIdx; i++){
+        if(max < arr[i]){
+            max = arr[i];
+            ret = i;
+        }
+    }
+    return ret;
+}
