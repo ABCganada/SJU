9b62a715a53325832c2c6379c76849946f17139f
ABCganada <shinmg9712@gmail.com>
2023-11-12 01:25:39 +0900

    알고리즘 - dfs

--- 2023-2/algorithm/11_dfs-and-bfs/prob1.c
+++ 2023-2/algorithm/11_dfs-and-bfs/prob1.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 typedef struct __vnode{
-    int n, visit;
+    int n, visit;   //0: init, 1: visited
     struct __inode *iList;
 }VNode;
 
 typedef struct __enode{
     int v1;
     int v2;
-    int visit;
+    int visit;  //0: init, 1: tree, -1: back
 }ENode;
 
 typedef struct __inode{
@@ -26,9 +26,15 @@ void initVertexList();
 void initEdgeList();
 
 INode *getINode(int i);
+int getOppositeVertexIndex(int eIdx, int vIdx);
 
 void insertEdge(int n1, int n2, int i);
-void insertIncident(VNode *v, int i);
+void insertIncident(int v, int i);
+
+void dfs(int n);
+
+void freeGraph();
+void freeIncident(int i);
 
 int main()
 {
@@ -43,6 +49,10 @@ int main()
         scanf("%d%d", &n1, &n2);
         insertEdge(n1, n2, i);
     }
+
+    dfs(s-1);
+    
+    freeGraph();
 
     return 0;
 }
@@ -66,22 +76,70 @@ INode *getINode(int i){
 
     return newNode;
 }
-void insertEdge(int n1, int n2, int i){
-    VNode *v1 = &vList[n1-1];
-    VNode *v2 = &vList[n2-1];
+int getOppositeVertexIndex(int eIdx, int vIdx){
+    int ret = eList[eIdx].v1;
+    if(ret == vIdx){
+        ret = eList[eIdx].v2;
+    }
 
+    return ret;
+}
+void insertEdge(int n1, int n2, int i){
     eList[i].v1 = n1-1;
     eList[i].v2 = n2-1;
     eList[i].visit = 0;
-    insertIncident(v1, i);
-    insertIncident(v2, i);
+    insertIncident(n1-1, i);
+    insertIncident(n2-1, i);
 }
-void insertIncident(VNode *v, int i){
-    INode *ptr = v->iList;
+void insertIncident(int v, int i){
+    INode *newNode = getINode(i);
+    int oppositeIdx = getOppositeVertexIndex(i, v);
+
+    INode *ptr = vList[v].iList;
+
     while(ptr->next){
+        if(getOppositeVertexIndex(ptr->next->eIdx, v) > oppositeIdx){
+            newNode->next = ptr->next;
+            ptr->next = newNode;
+            return;
+        }
         ptr = ptr->next;
     }
 
-    INode *newNode = getINode(i);
     ptr->next = newNode;
+}
+void dfs(int k){
+    vList[k].visit = 1;
+    printf("%d\n", k+1);
+
+    INode *ptr = vList[k].iList->next;
+    while(ptr){
+        if(eList[ptr->eIdx].visit == 0){
+            int oppositeV = getOppositeVertexIndex(ptr->eIdx, k);
+            if(vList[oppositeV].visit == 0){
+                eList[ptr->eIdx].visit = 1;
+                dfs(oppositeV);
+            } else{
+                eList[ptr->eIdx].visit = -1;
+            }
+        }
+        ptr = ptr->next;
+    }
+}
+void freeGraph(){
+    for(int i=0; i<n; i++){
+        freeIncident(i);
+    }
+
+    free(vList);
+    free(eList);
+}
+void freeIncident(int i){
+    INode *cur = vList[i].iList;
+
+    while(cur){
+        INode *next = cur->next;
+        free(cur);
+        cur = next;
+    }
 }
