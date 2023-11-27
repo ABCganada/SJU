6b570adb95913a32075cca216ab87da4d1d15c1b
ABCganada <shinmg9712@gmail.com>
2023-11-27 17:40:51 +0900

    알고리즘 - MST / Kruskal algorithm

--- 2023-2/algorithm/13_mst/prob2.c
+++ 2023-2/algorithm/13_mst/prob2.c
@@ -42,6 +42,7 @@ int isEmpty();
 int isInQueue(ENode *v);
 void swap(ENode **v1, ENode **v2);
 ENode *removeMin();
+void heapSort();
 
 void initVertexList();
 void initEdgeList();
@@ -83,21 +84,21 @@ void buildHeap(int i){
 void downHeap(int i){
     int leftChild;
     int rightChild;
-    int smallChild;
+    int bigChild;
 
     while(2*i < h.rear){
         leftChild = 2*i;
         rightChild = 2*i + 1;
 
         if(rightChild < h.rear){
-            smallChild = h.queue[leftChild]->w < h.queue[rightChild]->w ? leftChild : rightChild;
+            bigChild = h.queue[leftChild]->w > h.queue[rightChild]->w ? leftChild : rightChild;
         } else{
-            smallChild = leftChild;
+            bigChild = leftChild;
         }
 
-        if(h.queue[i]->w > h.queue[smallChild]->w){
-            swap(&h.queue[i], &h.queue[smallChild]);
-            i = smallChild;
+        if(h.queue[i]->w < h.queue[bigChild]->w){
+            swap(&h.queue[i], &h.queue[bigChild]);
+            i = bigChild;
         } else{
             break;
         }
@@ -124,13 +125,30 @@ void swap(ENode **v1, ENode **v2){
     *v2 = tmp;
 }
 ENode *removeMin(){
-    swap(&h.queue[h.head], &h.queue[h.rear - 1]);
-    h.rear--;
-
-    ENode *ret = h.queue[h.rear];
-    downHeap(1);
-
+    ENode *ret = h.queue[h.head++];
     return ret;
+}
+void heapSort(){
+    buildHeap(1);
+
+    
+    // printf("\nAfter build heap\n");
+    // for(int i=h.head; i<h.rear; i++){
+    //     printf("%d %d %d\n", h.queue[i]->vIdx1+1, h.queue[i]->vIdx2+1, h.queue[i]->w);
+    // }
+
+    int lastIdx = h.rear-1;
+    for(int i=lastIdx; i>=2; i--){
+        swap(&h.queue[1], &h.queue[i]);
+        h.rear--;
+        downHeap(1);
+    }
+
+    h.rear = lastIdx + 1;
+    // printf("\nAfter heap sort\n");
+    // for(int i=h.head; i<h.rear; i++){
+    //     printf("%d %d %d\n", h.queue[i]->vIdx1+1, h.queue[i]->vIdx2+1, h.queue[i]->w);
+    // }
 }
 void initVertexList(){
     for(int i=0; i<n; i++){
@@ -157,9 +175,39 @@ int kruskalMST(){
         h.queue[h.rear++] = &g.eList[i];
     }
 
-    buildHeap(1);
+    heapSort();
 
-    
+    while(!isEmpty()){
+        ENode *e = removeMin();
+        // printf("\nremoved e : %d %d %d\n", e->vIdx1+1, e->vIdx2+1, e->w);
+        int v1 = e->vIdx1;
+        int v2 = e->vIdx2;
+
+        // printf("Before > > >");
+        // for(int i=0; i<n; i++){
+        //     printf(" %d", g.vList[i].sack.sNum);
+        // }
+        // printf("\n");
+
+        if(g.vList[v1].sack.sNum != g.vList[v2].sack.sNum){
+            printf(" %d", e->w);
+            wSum += e->w;
+
+            int targetSackNum = g.vList[v2].sack.sNum;
+            for(int i=0; i<n; i++){
+                if(g.vList[i].sack.sNum == targetSackNum){
+                    g.vList[i].sack.sNum = g.vList[v1].sack.sNum;
+                }
+            }
+        }
+
+        // printf("After > > >");
+        // for(int i=0; i<n; i++){
+        //     printf(" %d", g.vList[i].sack.sNum);
+        // }
+        // printf("\n\n");
+    }
+    printf("\n");
 
     return wSum;
 }
